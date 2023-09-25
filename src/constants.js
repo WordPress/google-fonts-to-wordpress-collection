a6c6c96f09749e9a5e53f4ed92536c199583206c
Matias Benedetto <matias.benedetto@gmail.com>
2023-09-25 12:34:55 -0300

    renaming file

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const API_URL = "https://www.googleapis.com/webfonts/v1/webfonts?key=";
 const API_KEY = process.env.GOOGLE_FONTS_API_KEY;
 const GOOGLE_FONTS_FILE_PATH = "./output/google-fonts.json";
-const GOOGLE_FONTS_WITH_PREVIEWS_FILE_PATH = "./output/google-fonts-with-previews.json";
+const GOOGLE_FONTS_WITH_PREVIEWS_FILE_PATH = "./output/google-fonts-with-preview.json";
 const SVG_PREVIEWS_BASE_URL = "https://raw.githubusercontent.com/matiasbenedetto/google-fonts-to-wordpress-collection/main/output/previews/";
 
 module.exports = {
