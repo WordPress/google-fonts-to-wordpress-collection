e3152aa44c6b1629a0daa657a3b7729a849fff8c
Matias Benedetto <matias.benedetto@gmail.com>
2024-01-16 10:22:08 -0300

    format js source

--- src/download_google_font_assets.js
+++ src/download_google_font_assets.js
@@ -1,58 +1,70 @@
 /**
  * External dependencies
  */
-const fs = require('fs');
-const path = require("path");
+const fs = require( 'fs' );
+const path = require( 'path' );
 
 /**
  * Internal dependencies
  */
-const { GOOGLE_FONTS_FILE } = require('./constants');
-const { releasePath, downloadFile } = require('./utils');
-
+const { GOOGLE_FONTS_FILE } = require( './constants' );
+const { releasePath, downloadFile } = require( './utils' );
 
 function getFamilies() {
-    const googleFontsFile = fs.readFileSync(
-        releasePath(GOOGLE_FONTS_FILE),
-        "utf8"
-    );
-    const googleFonts = JSON.parse(googleFontsFile);
-    return googleFonts.font_families;
+	const googleFontsFile = fs.readFileSync(
+		releasePath( GOOGLE_FONTS_FILE ),
+		'utf8'
+	);
+	const googleFonts = JSON.parse( googleFontsFile );
+	return googleFonts.font_families;
 }
 
-
 async function downloadFontFamilies() {
-    const families = getFamilies();
-    let facesCount = 0;
-    let facesSuccessCount = 0;
+	const families = getFamilies();
+	let facesCount = 0;
+	let facesSuccessCount = 0;
 
-    for (let i = 0; i < families.length; i++) {
-        console.info(`ℹ️  Downloading ${families[i].font_family_settings.name} (${i + 1}/${families.length})`);
-        for (let x = 0; x < families[i].font_family_settings.fontFace.length; x++) {
-            facesCount++;
+	for ( let i = 0; i < families.length; i++ ) {
+		console.info(
+			`ℹ️  Downloading ${ families[ i ].font_family_settings.name } (${
+				i + 1
+			}/${ families.length })`
+		);
+		for (
+			let x = 0;
+			x < families[ i ].font_family_settings.fontFace.length;
+			x++
+		) {
+			facesCount++;
 
-            const url = families[i].font_family_settings.fontFace[x]['src'];
-            const relativePath = url.replace("https://fonts.gstatic.com/s/", "");
-            const destPath = "./" + path.join("font-assets/", relativePath);
+			const url =
+				families[ i ].font_family_settings.fontFace[ x ][ 'src' ];
+			const relativePath = url.replace(
+				'https://fonts.gstatic.com/s/',
+				''
+			);
+			const destPath = './' + path.join( 'font-assets/', relativePath );
 
-            try {
-                await downloadFile(url, destPath);
-                facesSuccessCount++;
-                console.log(`✅ Downloaded to ${destPath}`);
-            } catch (error) {
-                console.error(`❎  Failed to download ${url}: ${error}`);
-            }
-            console.log("");
-        }
+			try {
+				await downloadFile( url, destPath );
+				facesSuccessCount++;
+				console.log( `✅ Downloaded to ${ destPath }` );
+			} catch ( error ) {
+				console.error( `❎  Failed to download ${ url }: ${ error }` );
+			}
+			console.log( '' );
+		}
+	}
 
-    }
-
-    if (facesCount === facesSuccessCount) {
-        console.log(`🏅  Downloaded ${facesSuccessCount} of ${facesCount} font faces.`);
-    } else {
-        console.warn(`🚩  Downloaded ${facesSuccessCount} of ${facesCount} font faces. Check for errors.`);
-    }
-
+	if ( facesCount === facesSuccessCount ) {
+		console.log(
+			`🏅  Downloaded ${ facesSuccessCount } of ${ facesCount } font faces.`
+		);
+	} else {
+		console.warn(
+			`🚩  Downloaded ${ facesSuccessCount } of ${ facesCount } font faces. Check for errors.`
+		);
+	}
 }
 
 downloadFontFamilies();
